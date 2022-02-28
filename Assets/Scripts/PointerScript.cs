6fd26a47a5b48e307b8d5da641e7a7cb60197b1d
Danejw <djw.learn@gmail.com>
2022-02-27 17:16:25 -0800

    Update PointerScript.cs

--- Assets/Scripts/PointerScript.cs
+++ Assets/Scripts/PointerScript.cs
@@ -703,6 +703,7 @@ namespace TiltBrush
             return m_CurrentPressure;
         }
 
+        // maps one range into another
         float Remap(float value, float from1, float to1, float from2, float to2)
         {
             return (value - from1) / (to1 - from1) * (to2 - from2) + from2;          
